a2904d41b3d5fa24e92ac2f5a2876facbba66ab5
Gustavo Pereira <gustavo.s.pereira@nos.pt>
2023-03-09 10:22:41 +0000

    Create class for loading data from TSV file

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,10 +1,44 @@
 """Module for cleaning code and saving data transformed"""
 import argparse
 import pathlib
+from abc import ABC, abstractmethod
 import pandas as pd
 
 # Define path of files
 full_path: pathlib.Path = pathlib.Path(__file__).parent / 'data'
+
+class LoadData(ABC):
+    """
+    Define interface wich should be used by the different loader classes
+    independent from the type of file
+    """
+
+    @abstractmethod
+    def read_data(self, file_name: str, path: pathlib.Path):
+        """
+        Load data from file in path provided
+        """
+        pass
+
+class LoadDataTSV(LoadData):
+    """
+    Load data from a TSV file
+    """
+
+    def read_data(
+        self,
+        file_name: str,
+        path: pathlib.Path
+    ) -> pd.DataFrame:
+        """
+        Load specified file as a pandas DataFrame from data directory
+        :param file_name: Name of the file to be loaded
+        :param path_file: Path to the file
+
+        :return: Pandas DataFrame with loaded data
+        """
+
+        return pd.read_table(path / file_name)
 
 def load_data(
     file_name: str,
