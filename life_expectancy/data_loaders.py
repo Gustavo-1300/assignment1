9e683a007ab95155c03cad7a17633230aacee060
Gustavo Pereira <gustavo.s.pereira@nos.pt>
2023-03-09 11:04:40 +0000

    Class to load data in compressed format

--- life_expectancy/data_loaders.py
+++ life_expectancy/data_loaders.py
@@ -14,24 +14,43 @@ class LoadData(ABC):
         """
         Load data from file in path provided
         """
-        pass
 
 class LoadDataTSV(LoadData):
     """
     Load data from a TSV file
     """
 
+    @staticmethod
     def read_data(
-        self,
         file_name: str,
         path: pathlib.Path
     ) -> pd.DataFrame:
         """
         Load specified file as a pandas DataFrame from data directory
-        :param file_name: Name of the file to be loaded
+        :param file_name: Name of the file to be loaded in tsv format
         :param path_file: Path to the file
 
         :return: Pandas DataFrame with loaded data
         """
 
         return pd.read_table(path / file_name)
+
+class LoadDataComrpessed(LoadData):
+    """
+    Load JSON data from a compressed file
+    """
+
+    @staticmethod
+    def read_data(
+        file_name: str,
+        path: pathlib.Path
+    ) -> pd.DataFrame:
+        """
+        Load specified file as a pandas DataFrame from data directory
+        :param file_name: Name of the file to be loaded in json compressed format
+        :param path_file: Path to the file
+
+        :return: Pandas DataFrame with loaded data
+        """
+
+        return pd.read_json(path / file_name, compression='infer')
