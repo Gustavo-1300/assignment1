3072681542fb90297e89cd842546675436070d95
Gustavo Pereira <gustavo.s.pereira@nos.pt>
2023-03-09 18:23:16 +0000

    Test saving data

--- Tests/test_cleaning.py
+++ Tests/test_cleaning.py
@@ -56,29 +56,17 @@ def test_extract_floats(fixture_tabular, fixture_floats):
 
     pd.testing.assert_frame_equal(data_observed, fixture_floats)
 
-# def test_clean_data(fixture_raw, fixture_expect):
-#     """Run the `clean_data` function and compare the output to the expected output"""
+def test_save_data(fixture_expect, capfd):
+    """Run the 'save_data'"""
+    with patch.object(fixture_expect, 'to_csv') as to_csv_mock:
+        to_csv_mock.side_effect = print('Data saved', end="")
+        cleaning.save_data(
+            fixture_expect,
+            'pt_life_expectancy.csv',
+            OUTPUT_DIR
+        )
+        to_csv_mock.assert_called_once()
 
-#     pt_life_expectancy_data = clean_data(
-#         life_data=fixture_raw,
-#         region='PT'
-#     ).reset_index(drop=True)
-
-#     pd.testing.assert_frame_equal(
-#         pt_life_expectancy_data, fixture_expect
-#     )
-
-# def test_save_data(fixture_expect, capfd):
-#     """Run the 'save_data'"""
-#     with patch.object(fixture_expect, 'to_csv') as to_csv_mock:
-#         to_csv_mock.side_effect = print('Data saved', end="")
-#         save_data(
-#             fixture_expect,
-#             'pt_life_expectancy.csv',
-#             OUTPUT_DIR
-#         )
-#         to_csv_mock.assert_called_once()
-
-#         # capfd is a Pytest fixture that captures the output of the print statement
-#         out, _ = capfd.readouterr()
-#         assert out == "Data saved"
+        # capfd is a Pytest fixture that captures the output of the print statement
+        out, _ = capfd.readouterr()
+        assert out == "Data saved"
